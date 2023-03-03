4a77c27ea0b1e360fbc10909313f11ee6f838d02
lycHub <1120805364@qq.com>
2023-03-03 19:52:28 +0800

    rollup

--- packages/sortable/rollup.config.js
+++ packages/sortable/rollup.config.js
@@ -1,16 +1,33 @@
 import {defineConfig} from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import del from "rollup-plugin-delete";
 
-export default defineConfig({
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'esm',
-        entryFileNames: 'sortable-esm.js'
-    },
-    plugins: [
-        typescript({
-            tsconfig: 'tsconfig.json'
-        })
-    ]
-});
+let plugins = [
+    typescript({
+        tsconfig: 'tsconfig.json'
+    })
+];
+
+export default ({ configProd }) => {
+    if (configProd) {
+        plugins = [
+            del({ targets: 'dist' }),
+            ...plugins,
+            terser()
+        ]
+    }
+    return defineConfig({
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist',
+            format: 'esm',
+            entryFileNames: 'sortable-esm.js'
+        },
+        plugins,
+        watch: {
+            include: ["src/**/*.ts"],
+            buildDelay: 2000
+        }
+    });
+}
--- packages/sortable/src/index.ts
+++ packages/sortable/src/index.ts
@@ -1,7 +1,7 @@
 function plus(n1: number, n2: number) {
     return n1 + n2;
 }
-plus(2, 5);
+
 export {
     plus
 }
